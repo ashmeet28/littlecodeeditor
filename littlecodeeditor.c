eef4b53c3be701d315bc3ea8e639d91542caa452
ashmeet28 <112308196+ashmeet28@users.noreply.github.com>
2024-01-11 23:00:54 -0500

    Rederering all assci

--- littlecodeeditor.c
+++ littlecodeeditor.c
@@ -1,9 +1,22 @@
 #include "SDL.h"
 #include "SDL_ttf.h"
 #include <stdio.h>
+#include <stdlib.h>
 
 #define DEFAULT_WIN_WIDTH 1280
 #define DEFAULT_WIN_HEIGHT 720
+
+const char *get_all_assci_char() {
+    uint8_t *all_assci_char = (uint8_t*)malloc(96);
+    uint8_t c = 0x20;
+    while (c != 0x7f) {
+        *(all_assci_char + (c - 0x20)) = c;
+        c++;
+    }
+    *(all_assci_char + (c - 0x20)) = 0;
+    
+    return (const char*)all_assci_char;
+}
 
 int main(int argc, char* argv[]) {
     SDL_Init(SDL_INIT_VIDEO);
@@ -16,7 +29,7 @@ int main(int argc, char* argv[]) {
 
     TTF_Init();
 
-    TTF_Font *default_font = TTF_OpenFont("default-fonts/Hack-Regular.ttf", 40);
+    TTF_Font *default_font = TTF_OpenFont("default-fonts/Hack-Regular.ttf", 20);
 
     SDL_Color fg;
     SDL_Color bg;
@@ -29,8 +42,8 @@ int main(int argc, char* argv[]) {
     bg.b = 0xff;
     bg.a = 0xff;
 
-
-    SDL_Surface *font_surface = TTF_RenderUTF8_LCD(default_font, "Hello World", fg, bg);
+    
+    SDL_Surface *font_surface = TTF_RenderUTF8_LCD(default_font, get_all_assci_char(), fg, bg);
     SDL_Texture *font_texture = SDL_CreateTextureFromSurface(renderer, font_surface);
     SDL_Rect r;
 
